fcdbba41c685076d8bd4053c9578a46c87b1b85e
mbraddy <42819586+mbraddy@users.noreply.github.com>
2020-03-27 12:44:18 -0400

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,57 +1,5 @@
-// TicTacToe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "BoardState.h"
 #include "TicTacToe.h"
-
-//	Player # one or two takes a turn
-BoardState* TicTacToe::inputMove(string player, BoardState* bs) {
-	string turn;
-	int inputOne, inputTwo;
-
-	//	Clear screen and Displays game board
-	system("CLS");
-	bs->display();
-
-	//	Determines which player is placing
-	if (player.compare("one") == 0)
-		turn = "X";
-	else if (player.compare("two") == 0)
-		turn = "O";
-	else
-		return bs;
-
-	//	Enter loops to check validity of inputs
-	do {
-
-		//	Row
-		do {
-			cout << "\nPlayer " << player.c_str() << ", enter the row number (1, 2, or 3) for your next move: ";
-			cin >> inputOne;
-		} while (!validityCheck(inputOne));
-
-		//	Column
-		do {
-			cout << "\nPlayer " << player.c_str() << ", enter the column number (1, 2, or 3) for your next move: ";
-			cin >> inputTwo;
-		} while (!validityCheck(inputTwo));
-
-	//	Exits on validation
-	} while (!(bs->validateMove(inputOne - 1, inputTwo - 1)));
-
-	//	Input X or O
-	if (player.compare("one") == 0)
-		bs->setX(inputOne - 1, inputTwo - 1);
-	else
-		bs->setO(inputOne - 1, inputTwo - 1);
-
-	return bs;
-}
-
-//	Make sure the inputs are between 1 and 3
-bool TicTacToe::validityCheck(int test) {
-	return (test < 4 && test  > 0);
-}
 
 //	Main Menu for the game
 void displayMenu() {
@@ -83,7 +31,7 @@ int main() {
 	int victoryFlag = 0;
 	int playerOneScore = 0, playerTwoScore = 0;
 	int turnCounter = 0;
-	
+
 	//	Game loop to keep score and refresh board states when new games started.
 	do {
 		//	Main Menu display and selection
@@ -92,14 +40,14 @@ int main() {
 			displayMenu();
 			cin >> input;
 		} while (!validateMenuSelection(input));
-		
+
 		switch (input) {
-		//	Start a new game
+			//	Start a new game
 		case 'n':
 			//	Initialize a fresh board and the turn counter (for draws)
 			board = new BoardState;
 			turnCounter = 0;
-			
+
 			//	While the board isn't full and no one has won, alternate turns and check for victories.
 			while (victoryFlag == 0) {
 				turnCounter++;
@@ -134,14 +82,14 @@ int main() {
 			victoryFlag = 0;
 			break;
 
-		//	View the number of wins each player has obtained.
+			//	View the number of wins each player has obtained.
 		case 'v':
 			cout << "Player One has " << playerOneScore << " wins." << endl;
 			cout << "Player Two has " << playerTwoScore << " wins." << endl;
 			system("pause");
 			break;
 
-		//	Quit the game.
+			//	Quit the game.
 		case 'q':
 			quitFlag = true;
 			break;
